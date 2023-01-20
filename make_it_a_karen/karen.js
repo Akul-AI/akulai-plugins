200ca01909cc65b7807a43ddd6272c0c2190526d
Akul Goel <akulgoel2010@gmail.com>
2023-01-19 20:16:46 -0500

    Update karen.js

--- make_it_a_karen/karen.js
+++ make_it_a_karen/karen.js
@@ -1,5 +1,5 @@
 // Have fun dealing with this one...
 
 if (command.includes("be a karen")) {
-    akulai.speak("Don't you dare command me!");
+    akulai.speak("Don't you dare command me! I want to speak to the manager!");
 }
