f8e5d757993cddbdf1dd16a26478c11c4acb5986
Akul Goel <akulgoel2010@gmail.com>
2023-02-03 18:31:42 -0500

    Update exit.py

--- plugins/exit_app/exit.py
+++ plugins/exit_app/exit.py
@@ -1,3 +1,8 @@
 if "exit" or "quit" in command:
-  akulai.speak("Okay, please come again!")
-  exit()
+  akulai.speak("Are you sure?")
+  akulai.listen()
+  if command == "yes":
+    akulai.speak("Okay, please come again!")
+    exit()
+  else:
+    akulai.speak("Okay, what would you like to say?")
