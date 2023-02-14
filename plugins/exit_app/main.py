1be884b98669096857d1c6cf01b791019cfc1731
Akul Goel <akulgoel2010@gmail.com>
2023-02-13 19:52:46 -0500

    Delete main.py

--- plugins/exit_app/main.py
+++
@@ -1,8 +0,0 @@
-if "exit" or "quit" in command:
-  akulai.speak("Are you sure?")
-  akulai.listen()
-  if command == "yes":
-    akulai.speak("Okay, please come again!")
-    exit()
-  else:
-    akulai.speak("Okay, what would you like to say?")
