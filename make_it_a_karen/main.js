672df746d34297eb38f6136f2ecbd710a9c28e53
Akul Goel <akulgoel2010@gmail.com>
2023-07-30 01:31:49 +0200

    Delete all other plugins for now

--- make_it_a_karen/main.js
+++
@@ -1,6 +0,0 @@
-// Have fun dealing with this one...
-// I made it in JS just because I felt like I was going to make way too many Python plugins.
-
-if (command.includes("be a karen")) {
-    akulai.speak("Don't you dare command me! I want to speak to the manager!");
-}
